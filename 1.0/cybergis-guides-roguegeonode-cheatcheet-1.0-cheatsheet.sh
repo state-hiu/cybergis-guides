b72205b06a3ca6806f78b0441b41f54d8f127bf1
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-07-01 11:28:28 -0400

    Create cybergis-guides-roguegeonode-cheatcheet-1.0-cheatsheet.sh

---
+++ 1.0/cybergis-guides-roguegeonode-cheatcheet-1.0-cheatsheet.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+##This is a cheat sheet for the 
+
+#Step 1
+sudo su -
+apt-get update
+apt-get install -y curl vim git
+cd /opt
+git clone https://github.com/state-hiu/cybergis-scripts.git cybergis-scripts.git
+cp cybergis-scripts.git/profile/cybergis-scripts.sh /etc/profile.d/
+exit
+
+#Step 2
+sudo su -
+cybergis-script-init-rogue.sh prod user
+
+#Step 3
+cybergis-script-init-rogue.sh prod rvm
+reboot
+
+#Step 4
+sudo su -
+cybergis-script-init-rogue.sh prod gems
+
+#Step 5
+
+#Step 6
+sudo su -
+cybergis-script-init-rogue.sh prod geonode <fqdn>
