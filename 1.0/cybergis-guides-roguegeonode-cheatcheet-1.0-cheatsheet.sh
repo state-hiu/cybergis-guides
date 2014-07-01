9a62cb9a292e802240e6434aaeff2930361a9044
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-07-01 11:32:56 -0400

    Update cybergis-guides-roguegeonode-cheatcheet-1.0-cheatsheet.sh

--- 1.0/cybergis-guides-roguegeonode-cheatcheet-1.0-cheatsheet.sh
+++ 1.0/cybergis-guides-roguegeonode-cheatcheet-1.0-cheatsheet.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #============#
-#This is a cheat sheet for the ROGUE GeoNode Guide.
-#This file is designed to streamline installation without including explanation of each step.
-#Use the Guide found at https://github.com/state-hiu/cybergis-guides/blob/master/1.0/cybergis-guides-roguegeonode-1.0.md
+#This is the cheat sheet for the ROGUE GeoNode Guide.
+#This cheat sheet is designed to streamline installation without including explanation of each step.
+#Use the full guide found at https://github.com/state-hiu/cybergis-guides/blob/master/1.0/cybergis-guides-roguegeonode-1.0.md
 #if you wish to walk through the installation.
 #============#
 #Important!  This is not designed as a single bash script.  You'll still need to execute lines one by one.
