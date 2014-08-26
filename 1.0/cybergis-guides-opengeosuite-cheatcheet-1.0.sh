cefef9f1c8245e8a47fa55b8c6e33ec15ab116c4
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-08-26 13:54:37 -0400

    Update cybergis-guides-opengeosuite-cheatcheet-1.0.sh

--- 1.0/cybergis-guides-opengeosuite-cheatcheet-1.0.sh
+++ 1.0/cybergis-guides-opengeosuite-cheatcheet-1.0.sh
@@ -8,6 +8,22 @@
 #Important!  This is not designed as a single bash script.  You'll still need to execute lines one by one.
 #============#
 #Step 1
-
+apt-get update
+apt-get install -y curl vim git
+cd /opt
+git clone https://github.com/state-hiu/cybergis-scripts.git cybergis-scripts.git
+cp cybergis-scripts.git/profile/cybergis-scripts.sh /etc/profile.d/
 
 #Step 2
+wget -qO - http://apt.opengeo.org/gpg.key | apt-key add -
+echo "deb http://apt.opengeo.org/suite/v4/ubuntu/ precise main" > /etc/apt/sources.list.d/opengeo.list
+apt-get update
+
+#Step 3
+apt-get install opengeo
+
+#Step 4
+
+#Step 5
+cybergis-script-geoserver.sh prod tune <repo> <Xmx>
+cybergis-script-ec2.sh prod swap 64g /mnt/swap_64g.swap
