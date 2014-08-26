5c1dfa3810a1db33b1fb053e4e43a3db8787f019
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-08-26 13:49:27 -0400

    Create cybergis-guides-opengeosuite-cheatcheet-1.0.sh

---
+++ 1.0/cybergis-guides-opengeosuite-cheatcheet-1.0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#============#
+#This is the cheat sheet for the OpenGeo Suite Guide.
+#This cheat sheet is designed to streamline installation without including explanation of each step.
+#Use the full guide found at https://github.com/state-hiu/cybergis-guides/blob/master/1.0/cybergis-guides-opengeosuite-1.0.md
+#if you wish to walk through the installation.
+#============#
+#Important!  This is not designed as a single bash script.  You'll still need to execute lines one by one.
+#============#
+#Step 1
+
+
+#Step 2
